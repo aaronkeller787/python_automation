e8e2871bc12a809ca60f505f6b5f29e7fda6a073
Aaron Keller <1647490-aaron.keller787@users.noreply.gitlab.com>
2021-08-03 20:00:50 -0700

    testing .gitignore

--- settings.py
+++
@@ -1,7 +0,0 @@
-SERVERNAME='smtp.test.com'
-SERVERPORT=587
-USERNAME='example@example.com'
-PASSWRD='SuperSecretPassword'
-SUB='Domain Expiry List'
-SENDER='sender@example.com'
-RECEIVER='receiver@example.com'
